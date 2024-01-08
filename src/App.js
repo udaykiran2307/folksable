ee8f4ef4b390be7d4d8ad39cc1d40d7753001cbc
uday <udaykiranvalaboju@gmail.com>
2024-01-08 16:25:43 +0530

    focusComponent

--- src/App.js
+++ src/App.js
@@ -2,47 +2,43 @@ import logo from "./logo.svg";
 import "./App.css";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-function App() {
+import FocusComponent from "./Components/FocusComponent";
+const AppStoreComponent = () => {
   return (
-    <div>
-      {/* <div>
-        <img src="Logo_WBorderfolksableIcon.png" alt="icon"></img>
-        <h2>the modern struggle.</h2>
-        <p>
-          “All of humanity's problems stem from man's inability to sit quietly
-          in a room alone.” ~ Blaise Pascal, Pensées
-        </p>
-      </div> */}
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "space-evenly",
-              alignItems: "center",
-            }}
-          >
-            <img
-              src="Logo_WBorderfolksableIcon.png"
-              alt="icon"
-              style={{ width: "156px", height: "169px" }}
-            ></img>
-            <h2>the modern struggle.</h2>
-            <p
-            style={{alignContent:'center'}}
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src="Logo_WBorderfolksableIcon.png"
+            alt="icon"
+            style={{ width: "156px", height: "169px" }}
+          ></img>
+          <h2>the modern struggle.</h2>
+          <p style={{ alignContent: "center" }}>
+            “All of humanity's problems stem from man's inability to sit quietly
+            in a room alone.”
+          </p>
+          <h4 style={{ marginTop: "-1rem" }}> ~ Blaise Pascal, Pensées</h4>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignItems: "center",
+                gap: "20px",
+                justifyContent: "center",
+              }}
             >
-              “All of humanity's problems stem from man's inability to sit
-              quietly in a room alone.”
-            </p>
-            <h4 style={{marginTop:'-1rem'}}> ~ Blaise Pascal, Pensées</h4>
-            <div style={{ display: "flex" }}>
-              <div
-              style={{display:'flex', flexWrap:'wrap', alignItems:'center',gap:'20px',justifyContent:'center'}}
-              >
               <img
                 src="Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917 1appstore.png"
                 alt="appstore"
@@ -53,32 +49,41 @@ function App() {
                 alt="playstore"
                 style={{ width: "230px", height: "68px" }}
               ></img>
-              </div>
             </div>
-          </Grid>
-          <Grid
-            item
-            xs={12}
-            md={6}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              alignContent:'center'
-            }}
-          >
-            <img
-              src="Frame 275.png"
-              alt="card"
-              style={{
-                minHeight: "500px",
-                maxWidth: "500px",
-                alignContent: "center",
-              }}
-            ></img>
-          </Grid>
+          </div>
         </Grid>
-      </Box>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <img
+            src="Frame 275.png"
+            alt="card"
+            style={{
+              maxHeight: "500px",
+              maxWidth: "500px",
+              alignContent: "center",
+              width: "100%",
+            }}
+          ></img>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+function App() {
+  return (
+    <div>
+    <AppStoreComponent/>
+    <FocusComponent/>
     </div>
   );
 }
---
+++ src/Components/FocusComponent.js
@@ -0,0 +1,64 @@
+import { Box, Grid } from "@mui/material";
+import React from "react";
+
+const FocusComponent = () => {
+  return (
+    <Box sx={{ flexGrow: 1, marginTop:'3rem'}}>
+      <Grid container spacing={1}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            display:"flex",
+            justifyContent:'center',
+            alignItems:'center',
+            alignContent:'center'
+          }}
+        >
+          <div
+           style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign:'center',
+            width:'100%'
+          }}
+          >
+          <h2 style={{marginBottom:'-1.3rem'}}>Focus on</h2>
+          <h2 style={{marginBottom:'1rem'}}>what matters to you.</h2>
+          <p>
+            “Discover the power of reward design working for you, rather than
+            keeping you hooked.”
+          </p>
+          </div>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            alignContent: "center",
+          }}
+        >
+          <img
+            src="Group 7.png"
+            alt="group7"
+            style={{
+              maxHeight: "400px",
+              maxWidth: "400px",
+              alignContent: "center",
+              width: "100%",
+            }}
+          ></img>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
+
+export default FocusComponent;
